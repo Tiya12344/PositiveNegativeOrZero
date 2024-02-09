74413b80c0654618c0d53358f8ac5c0e60c217aa
Tiya Noonia <tiyanoonia2901@gmail.com>
2024-02-09 16:58:49 +0000

    my first commit to positive negative or zero

---
+++ main.py
@@ -0,0 +1,7 @@
+number = int(input("pick a number: "))
+if number > 0 :
+    print ("Your number is positive")
+elif number < 0 :
+    print("your number is negative")
+else:
+    print("your number is 0")
